4323c7dd449540e8a1672168c890de96c5a4e135
jonghun <jonghuncu@gmail.com>
2023-01-23 15:58:45 +0900

    test : HelloResponseDto 테스트 코드

---
+++ src/main/java/com/rivkode/springwebservice/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.rivkode.springwebservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
